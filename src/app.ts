1922be8868c72719696a82ed6a8005ccfd2a3538
aviel <aviel469@gmail.com>
2024-11-10 11:49:37 +0200

    conreoller app and add app.use

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express"
 import "dotenv/config"
+import userController  from "./controllers/users"
+import adminController  from "./controllers/admin"
+import candidatesController  from "./controllers/candidates"
+import votesController  from "./controllers/votes"
 
 const PORT = process.env.PORT || 3000
 const app =express()
 
+app.use(express.json())
+app.use("/api/users",userController)
+app.use("/api/admin",adminController)
+app.use("/api/votes",votesController)
+app.use("/api/candidates",candidatesController)
 
 app.listen(PORT,()=>{console.log(`Server started ,Visit "http://localhost:${PORT}"`)})
