51bf3f911c046420c058d915d84331b1b0fca152
aviel <aviel469@gmail.com>
2024-11-10 15:04:16 +0200

    login and egister *CRUD ONLY* no jwt

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,18 +1,29 @@
 import { Request, Response } from "express";
+import { createNewUser, userLogin } from "../services/userService";
+import { LoginDto, RegisterDto } from "../types/dto/userDto";
 
-export const login = async(req:Request,res:Response)=>{
+export const login = async(req:Request<LoginDto>,res:Response)=>{
     try {
-        
-    } catch (error) {
-        
+        const result =  await userLogin(req.body)
+        res.status(200).json({result})
+    } catch (err) {
+        res.status(400).json(err)
     }
 }
 
 
-export const register = async(req:Request,res:Response)=>{
+export const register = async(req:Request<RegisterDto>,res:Response)=>{
     try {
+        const result = await createNewUser(req.body)
+        res.status(201).json({
+            result
+        })
         
-    } catch (error) {
+    } catch (err) {
+        res.status(500).json({
+            err
+        })
+        console.log(err);
         
     }
 }
---
+++ src/services/userService.ts
@@ -0,0 +1,28 @@
+import { compare, hash } from "bcrypt";
+import User, { IUser } from "../models/user";
+import { LoginDto, RegisterDto } from "../types/dto/userDto";
+
+
+
+export const userLogin = async (user:LoginDto) => {
+    try {
+        const userFromDataBase = await User.findOne({username:user.username})
+        if(!userFromDataBase) throw new Error("user was not found")
+        const match = await compare(user.password,userFromDataBase.password)
+        if(!match) throw new Error("wrong password")
+        return userFromDataBase
+    } catch (err) {
+        return err
+    }
+}
+
+export const createNewUser = async(user:RegisterDto) => {
+    try {
+        const encPass = await hash(user.password, 10)
+        user.password = encPass
+        const newUser = new User(user)
+        return await newUser.save()
+    } catch (error) {
+       throw new Error("can't create a new user")
+    }
+}
