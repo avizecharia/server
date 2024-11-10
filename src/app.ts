206040880d60c47853a0c7722ace813e5bd7324d
aviel <aviel469@gmail.com>
2024-11-10 17:52:58 +0200

    formater

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,24 @@
-import express from "express"
-import "dotenv/config"
-import cors from "cors"
-import userController  from "./controllers/users"
-import adminController  from "./controllers/admin"
-import candidatesController  from "./controllers/candidates"
-import votesController  from "./controllers/votes"
-import connectToMongo from "./config/db"
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import userController from "./controllers/users";
+import adminController from "./controllers/admin";
+import candidatesController from "./controllers/candidates";
+import votesController from "./controllers/votes";
+import connectToMongo from "./config/db";
 
-const PORT = process.env.PORT || 3000
-const app =express()
-connectToMongo()
+const PORT = process.env.PORT || 3000;
+const app = express();
+connectToMongo();
 
-app.use(express.json())
-app.use({
-    
-})
+app.use(express.json());
+app.use(cors());
 
+app.use("/api/users", userController);
+app.use("/api/admin", adminController);
+app.use("/api/votes", votesController);
+app.use("/api/candidates", candidatesController);
 
-app.use("/api/users",userController)
-app.use("/api/admin",adminController)
-app.use("/api/votes",votesController)
-app.use("/api/candidates",candidatesController)
-
-app.listen(PORT,()=>{console.log(`Server started ,Visit "http://localhost:${PORT}"`)})
+app.listen(PORT, () => {
+  console.log(`Server started ,Visit "http://localhost:${PORT}"`);
+});
